6cda4ae78e3a455d111143b797b6925bcb75d029
Rafael_Deiro <rafaeldeirolopes@gmail.com>
2019-03-08 00:53:08 -0300

    First Commit. Edited README for describing the repository; Added VS Code Workspace to the root folder; Created Database->DAO->PDO file with a brief description and identification inside.

---
+++ Database/DAO/PDO/dao.php
@@ -0,0 +1,12 @@
+<?php
+/*
+
+Data Access Object with PDO (PHP Data Object)
+PHP 7.3.2
+
+Author: Rafael Deiro
+Date: 2019 March 8th
+Description: DAO for safe access to relational databases throught PDO. Requires config file for the DB credentials and other informations.
+
+*/
+?>
