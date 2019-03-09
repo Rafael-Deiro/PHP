2ecd8137ec238481d6eca7502b3f16db263f514c
Rafael-Deiro <rafaeldeirolopes@gmail.com>
2019-03-09 00:00:32 -0300

    Additions in Database->DAO->PDO
    Created db_config.php wich deals with database credentials and other sensitive data around the databases.
    Added get_credentials() and default() to db_config, wich get the credentials based on the token. default() for no token, get_credentials() with token.
    Added MySQL class to dao.php, wich handles MySQL interactions throught PDO.
    Started MySQL->__construct() wich will connect to the database (this may be delegated to a new function such as main() or connect() soon).
    
    TODO (imediate):
    - Database connection in dao.php->MySQL
    - Add and seek for credentials in storage file(s) in db_config.php

--- Database/DAO/PDO/dao.php
+++ Database/DAO/PDO/dao.php
@@ -9,4 +9,27 @@ Date: 2019 March 8th
 Description: DAO for safe access to relational databases throught PDO. Requires config file for the DB credentials and other informations.
 
 */
+
+// Imports db_config.php, who stores sensitive data such as db names, passwords, hosts, usernames, tokens, etc.
+require_once ('db_config.php');
+
+// MySQL class that extends db_config for the database information
+class MySQL extends db_config{
+
+  // Database connection object
+  private $conn;
+
+  // Connection to the DB when instantiating the class
+  private function __construct($db_token = null){
+
+    /*
+    $db_token stands for an encrypted identifier for the DB when the app uses more than one.
+    When the token is not sent, the default DB is used (set in db_config.php or storage file(s)).
+    */
+
+    $credentials = $this->main($db_token);
+
+    // TODO: Database connection
+  }
+}
 ?>
---
+++ Database/DAO/PDO/db_config.php
@@ -0,0 +1,119 @@
+<?php
+/*
+
+Database configuration file
+PHP 7.3.2
+
+Author: Rafael Deiro
+Date: 2019 March 8th
+Description: Configuration file that delivers sensitive information about the application databases (primarily credentials).
+
+*/
+
+// db_config class for storing the databases configurations
+class db_config{
+
+  // __construct() calls and returns main() when the class is instantiated
+  public function __construct($token = null){
+    return $this->main($token);
+  }
+
+  // main() handles the credentials delivery
+  protected function main($token = null){
+
+    // If a token is received, the function retrieves the respective credentials and returns it.
+    if ($token){
+      return $this->get_credentials($token);
+    }
+
+    // If no token is received, the function retrieves the default database credentials and returns it.
+    else {
+      return $this->default();
+    }
+  }
+
+  // Default database credentials function
+  private function default(){
+
+    // Credentials array
+    $credentials = array(
+      'DB_NAME' => 'corp_database',
+      'HOST' => '192.168.1.1:1234',
+      'USERNAME' => 'corp_user',
+      'PASSWORD' => 'evilcorp123'
+    );
+
+    /*
+    // If no credentials are set in the function
+    if (empty($credentials)){
+
+      // Fetches default credentials
+      $credentials = $this->fetch_credentials();
+
+      // If there are still no credentials, the function returns false
+      if (empty($credentials)){
+        return false;
+      }
+    }
+    */
+
+    // Returns the credentials
+    return $credentials;
+  }
+
+  // Token related database credentials function
+  private function get_credentials($token){
+
+    /*
+    // Fetches saved credentials
+    $file_credentials = $this->fetch_credentials($token);
+
+    // Merges the function credentials array with the fetched credentials array
+    array_merge($credentials, $file_credentials);
+    */
+
+    // Credentials array
+    $credentials = array(
+
+      // Indexed by the tokens
+      'TOKEN_1' => array(
+        'DB_NAME' => 'corp_database',
+        'HOST' => '192.168.1.1:1234',
+        'USERNAME' => 'corp_user',
+        'PASSWORD' => 'evilcorp123'
+      ),
+      'TOKEN_2' => array(
+        'DB_NAME' => 'corp_database_2',
+        'HOST' => '192.168.10.1:5678',
+        'USERNAME' => 'corp_user_2',
+        'PASSWORD' => 'evilcorp123'
+      ),
+      'TOKEN_3' => array(
+        'DB_NAME' => 'corp_database_3',
+        'HOST' => '192.168.0.1:4321',
+        'USERNAME' => 'corp_user_3',
+        'PASSWORD' => 'evilcorp123'
+      )
+    );
+
+    // Token check
+    if (array_key_exists($token, $credentials)){
+
+      // If the token is valid, the function returns the respective credentials
+      return $credentials[$token];
+    }
+    else {
+
+      // If the token is invalid, the function returns null
+      return false;
+    }
+  }
+
+  /*
+  // Seek in the storage file(s) for credentials
+  private function fetch_credentials($token = null){
+
+  }
+  */
+}
+?>
